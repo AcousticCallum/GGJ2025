3d5a4e8e8880c7e02039548dff79db7ec4e087d4
EAJB <edbuckett11@gmail.com>
2025-01-25 19:22:05 +0000

    Underwater Fog

---
+++ Assets/Scripts/Underwater.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Underwater : MonoBehaviour
+{
+    private void OnTriggerEnter(Collider other)
+    {
+        RenderSettings.fog = true;
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        RenderSettings.fog = false;
+    }
+}
